58bf29a31fbdcbe619a0348039b51173cc1fb856
James Dingle <jamesd@amdigital.co.uk>
2025-08-03 23:42:33 +0100

    feat: create and execute mutation operations
    code copied from original prototype

---
+++ RazorQuery/Mutation.cs
@@ -0,0 +1,51 @@
+﻿namespace RazorQuery;
+
+/// <summary>
+/// Represents an asynchronous operation that can mutate data based on the provided input.
+/// </summary>
+/// <remarks>This class encapsulates a mutation operation, allowing the caller to execute the operation
+/// asynchronously and track its status.
+/// </remarks>
+/// <typeparam name="TInput">The type of input data to be processed by the mutation.</typeparam>
+public class Mutation<TInput> : MutationBase
+{
+    private readonly Func<TInput, QueryFunctionContext, Task> _mutationFunc;
+
+    public Mutation(Func<TInput, QueryFunctionContext, Task> mutateFunction)
+    {
+        _mutationFunc = mutateFunction ?? throw new ArgumentNullException(nameof(mutateFunction));
+    }
+
+    public async Task Execute(TInput input)
+    {
+        Error = null;
+        Status = MutationStatus.Pending;
+
+        // TODO: Link this up to DI container or HttpClient factory
+        var funcContext = new QueryFunctionContext
+        {
+            ErrorMessage = string.Empty,
+            HttpClient = new HttpClient() // this is v bad, should be injected instead. just using for prototype.
+        };
+
+        try
+        {
+            await _mutationFunc(input, funcContext);
+
+            // check for errors raised by the function itself.
+            // TODO: currently using exceptions for flow logic, but should be 
+            //       replaced with a more structured error handling approach
+            if (funcContext.ErrorMessage.Length > 0)
+            {
+                throw new Exception(funcContext.ErrorMessage);
+            }
+
+            Status = MutationStatus.Success;
+        }
+        catch (Exception e)
+        {
+            Status = MutationStatus.Error;
+            Error = e;
+        }
+    }
+}
---
+++ RazorQuery/MutationBase.cs
@@ -0,0 +1,28 @@
+﻿namespace RazorQuery;
+
+
+public abstract class MutationBase
+{
+    public MutationStatus Status { get; protected set; } = MutationStatus.Idle;
+
+    public Exception? Error { get; protected set; } = null;
+
+    public bool IsIdle => Status == MutationStatus.Idle;
+
+    public bool IsPending => Status == MutationStatus.Pending;
+
+    public bool IsSuccess => Status == MutationStatus.Success;
+
+    public bool IsError => Status == MutationStatus.Error;
+}
+
+/// <summary>
+/// Represents the current state of a mutation.
+/// </summary>
+public enum MutationStatus
+{
+    Idle,
+    Pending,
+    Success,
+    Error
+}
