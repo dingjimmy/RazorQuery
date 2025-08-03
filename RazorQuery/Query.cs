4cc80500b36bc560ef317178c0ff318181dd2851
James Dingle <jamesd@amdigital.co.uk>
2025-08-03 23:41:52 +0100

    feat: create and execute query operations
    code copied from original prototype

---
+++ RazorQuery/Query.cs
@@ -0,0 +1,61 @@
+﻿namespace RazorQuery;
+
+/// <summary>
+/// Represents an asynchronous operation that can retrieve data that meets a specific criteria.
+/// </summary>
+/// <remarks>This class encapsulates a data retrieval operation, allowing the caller to execute the operation
+/// asynchronously and track its status.
+/// </remarks>
+/// <typeparam name="T">The type of data to be retrieved.</typeparam>
+/// <typeparam name="TFilter">The type representing the criteria used to filter the data being retrieved.</typeparam>
+public class Query<T, TFilter> : QueryBase
+    where T : new()
+{
+    private readonly Func<TFilter, QueryFunctionContext, Task<T>> _queryFunc;
+
+
+    public T Data { get; protected set; }
+
+
+    public Query(Func<TFilter, QueryFunctionContext, Task<T>> queryFunction)
+    {
+        Data = new T();
+        _queryFunc = queryFunction ?? throw new ArgumentNullException(nameof(queryFunction));
+    }
+
+    public async Task<T> Execute(TFilter filter)
+    {
+        Error = null;
+        Status = QueryStatus.Pending;
+
+
+        // TODO: Link this up to DI container or HttpClient factory
+        var funcContext = new QueryFunctionContext
+        {
+            ErrorMessage = string.Empty,
+            HttpClient = new HttpClient() // this is v bad, should be injected instead. just using for prototype.
+        };
+
+        try
+        {
+            Data = await _queryFunc(filter, funcContext);
+
+            // check for errors raised by the function itself.
+            // TODO: currently using exceptions for flow logic, but should be 
+            //       replaced with a more structured error handling approach
+            if (funcContext.ErrorMessage.Length > 0)
+            {
+                throw new Exception(funcContext.ErrorMessage);
+            }
+
+            Status = QueryStatus.Success;
+            return Data;
+        }
+        catch (Exception e)
+        {
+            Status = QueryStatus.Error;
+            Error = e;
+            return default!;
+        }
+    }
+}
---
+++ RazorQuery/QueryBase.cs
@@ -0,0 +1,49 @@
+﻿namespace RazorQuery;
+
+/// <summary>
+/// Represents the base class for query operations, providing common properties and 
+/// status checks.
+/// </summary>
+/// <remarks>This class serves as a foundation for implementing query-related 
+/// functionality. It includes properties to track the query's status, any associated 
+/// error, and convenience properties to simplify Razor Component code. Derived classes 
+/// should implement specific query logic while utilizing the provided status and 
+/// error tracking.</remarks>
+public abstract class QueryBase
+{
+    public QueryStatus Status { get; protected set; } = QueryStatus.Idle;
+
+    public Exception? Error { get; protected set; } = default;
+
+
+    public bool IsIdle => Status == QueryStatus.Idle;
+
+    public bool IsPending => Status == QueryStatus.Pending;
+
+    public bool IsSuccess => Status == QueryStatus.Success;
+
+    public bool IsError => Status == QueryStatus.Error;
+}
+
+/// <summary>
+/// Represents the context for executing a query function. Intended to be used by query 
+/// functions to access necessary resources and for reporting errors.
+/// </summary>
+public class QueryFunctionContext
+{
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public HttpClient HttpClient { get; set; }
+
+}
+
+/// <summary>
+/// Represents the current state of a query.
+/// </summary>
+public enum QueryStatus
+{
+    Idle,
+    Pending,
+    Success,
+    Error
+}
